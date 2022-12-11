bb95226a71126af26c7b2d7fabdaa6764feeb794
Matt <mattpuchalski5@gmail.com>
2022-12-10 19:15:10 -0500

    that's it for tonight
    calling it quits - Last Crusade is on TV

--- python/text_generator.py
+++ python/text_generator.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
 # this is our sentence generator that smashes together our people/places/things from our CSV dictionary
 # imports
 import pandas as pd
 import os
 
 
+## TESTS TEST TEST
+
+df = pd.read_csv('~/programming/personalized_art_generation/prompts/matts_list.csv')
+#might be crap
+
+print(df.to_string())
+# TEST TEST TEST
